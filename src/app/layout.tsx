db8fa8d7fcb5c45dd45c7ee61501e697b736dc18
David Costa <davidcostadev@gmail.com>
2023-03-15 21:40:01 -0300

    Improving setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import 'assets/global.css';
+
+export const metadata = {
+  title: '@davidcostadev',
+  description: "David Costa home's page",
+};
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-white">{children}</body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
 export default function Index() {
-  const a = 1;
+  const a = 3;
   return <div>Hello world!{a}</div>;
 }
