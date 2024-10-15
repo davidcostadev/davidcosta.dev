8c61c6cc5c591fc94c2fbabaad47cf5d9fbb6bb4
David Costa <davidcostadev@gmail.com>
2024-10-15 15:01:33 -0300

    fix: simplify RootLayout component by removing locale prop

--- src/app/[locale]/articles/layout.tsx
+++ src/app/[locale]/articles/layout.tsx
@@ -8,17 +8,10 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-  locale,
-}: {
-  children: React.ReactNode;
-  locale: string;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="px-5 sm:px-12 max-w-4xl mx-auto min-h-screen">
       <article className="break-words text-primary-light dark:text-primary-dark font-normal">
-        {locale}
         {children}
       </article>
     </main>
